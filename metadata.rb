a80d0a1260f1737b56e403400ba204a56de1ea1b
danielristic <danieldristic@gmail.com>
2019-08-28 13:48:05 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'ben@talkingquickly.co.uk'
 license          'All rights reserved'
 description      'Installs/Configures redis-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends          'apt'
